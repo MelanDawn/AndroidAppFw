d8ea3ab33b3a4caf246c295878adae1ca762a216
zhangs <ztzhangshu@hotmail.com>
2019-02-21 14:26:37 +0800

    [R&D_Center] 首次添加TextView样例
    修改原因: Feature
    应用版本: 1.0
    问题原因:
    修复方式：

--- app/src/main/java/com/zs/androidappfw/ui/activity/MainActivity.java
+++ app/src/main/java/com/zs/androidappfw/ui/activity/MainActivity.java
@@ -1,6 +1,12 @@
 package com.zs.androidappfw.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.zs.androidappfw.R;
 
 /**
  * Created by shoes on 2017/10/25.
@@ -8,4 +14,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+
+    public void toTextView(View view) {
+        startActivity(new Intent(MainActivity.this, TextViewAct.class));
+    }
 }
