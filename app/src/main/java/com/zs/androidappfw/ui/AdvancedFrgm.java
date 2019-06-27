9784be92caf9b9dc8f3ff05f48e638d804114103
zhangs <ztzhangshu@hotmail.com>
2019-06-27 13:41:48 +0800

    [R&D_Center] 添加 Android TouchEvent 事件分发机制方法调用栈样例
    修改原因: Feature
    应用版本: 1.0
    问题原因:
    修复方式：

--- app/src/main/java/com/zs/androidappfw/ui/AdvancedFrgm.java
+++ app/src/main/java/com/zs/androidappfw/ui/AdvancedFrgm.java
@@ -8,13 +8,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.zs.androidappfw.R;
+import com.zs.androidappfw.ui.activity.advanced.CustomizationPermissionAct;
+import com.zs.androidappfw.ui.activity.advanced.TouchEventDispatchAct;
+import com.zs.androidappfw.ui.activity.view.AbsSeekBarAct;
+import com.zs.androidappfw.ui.activity.view.ProgressBarAct;
 import com.zs.androidappfw.ui.base.BaseFragment;
 
-public class AdvancedFrgm extends BaseFragment {
+public class AdvancedFrgm extends BaseFragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frgm_main_advanced, container, false);
+        View view = inflater.inflate(R.layout.frgm_main_advanced, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        initAndSetClickListener(view, R.id.advanced_to_diy_permission);
+        initAndSetClickListener(view, R.id.advanced_to_dispatch_touch_event);
+    }
+
+    private void initAndSetClickListener(View view, int id) {
+        view.findViewById(id).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.advanced_to_diy_permission:
+                startActivity(CustomizationPermissionAct.class);
+                break;
+            case R.id.advanced_to_dispatch_touch_event:
+                startActivity(TouchEventDispatchAct.class);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/zs/androidappfw/ui/activity/advanced/TouchEventDispatchAct.java
@@ -0,0 +1,39 @@
+package com.zs.androidappfw.ui.activity.advanced;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.MotionEvent;
+
+import com.zs.androidappfw.R;
+import com.zs.androidappfw.ui.base.BaseActivity;
+import com.zs.androidappfw.utils.LUtil;
+
+public class TouchEventDispatchAct extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_touch_event_dispatch);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        LUtil.d(TAG, "dispatchTouchEvent start, " + ev.toString());
+        boolean result = super.dispatchTouchEvent(ev);
+        LUtil.d(TAG, "dispatchTouchEvent end: " + result);
+        return result;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        LUtil.d(TAG, "onTouchEvent start, " + ev.toString());
+        boolean result = super.onTouchEvent(ev);
+        LUtil.d(TAG, "onTouchEvent end: " + result);
+        return result;
+    }
+}
+
+
+
+
+
